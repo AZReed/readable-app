73ddad0e2bb1df8fac6b961ce9628eb1be838fe9
AZReed <azreed@gmail.com>
2017-10-17 16:11:48 -0300

    feat: basic shape

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,9 +1,68 @@
 import React, { Component } from "react";
+import { fetchCategories } from "../actions";
+import { connect } from "react-redux";
 
 class Categories extends Component {
+  state = {
+    indexActive: -1
+  }
+
+  componentDidMount() {
+    this.props.fetchCategories();
+  }
+
+  isActive(index) {
+    if (index === this.state.indexActive) {
+      return "panel-block is-active";
+    }
+    return "panel-block";
+  }
+
+  handleCategory(e, category, index) {
+    console.log(e.target, category)
+    this.setState({indexActive: index})
+  }
+
   render() {
-    return <div>Categories</div>;
+
+    return (
+      <nav className="panel">
+        <p className="panel-heading">Readable</p>
+
+        <p className="panel-tabs">
+          <a className="is-active">categories</a>
+          <a>filters</a>
+        </p>
+
+        {this.props.categories.map((category, index) => (
+          <a key={category.name} onClick={(e) => this.handleCategory(e, category.path, index)} className={this.isActive(index)}>
+            <span className="panel-icon">
+              <i className="fa fa-book" />
+            </span>
+            {category.name}
+          </a>
+        ))}
+
+        <div className="panel-block">
+          <button className="button is-link is-outlined is-fullwidth">
+            reset all filters
+          </button>
+        </div>
+      </nav>
+    );
   }
 }
 
-export default Categories;
+function mapStateToProps({ categories }) {
+  return {
+    categories: categories.allCategories || []
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    fetchCategories: () => dispatch(fetchCategories())
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Categories);
